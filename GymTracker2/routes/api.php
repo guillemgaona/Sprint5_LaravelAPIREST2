e4241b95c0b21c14e337800488f3b08bbf6ecd20
guillemgaona <guillemgaona@hotmail.com>
2025-06-01 11:26:55 +0200

    use controllers

--- GymTracker2/routes/api.php
+++ GymTracker2/routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ExerciseController;
+use App\Http\Controllers\Api\TrainingSessionController;
+use App\Http\Controllers\Api\SetController;
+use App\Http\Controllers\Api\StatsController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
